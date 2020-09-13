8427488bf3dfb0afead94f9cdd9fa861b4bfd53a
RuilingGuo <ruiling.guo1@thoughtworks.com>
2020-09-13 14:41:48 +0800

    [ruiling] feat: finish problem1

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +17,9 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+  @JsonFormat(shape = JsonFormat.Shape.NUMBER, pattern="s")
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
+
 }
